0b6560cbd7266688c4746adc63467bb423dcbf74
DLdevcoder <dtlong1410@gmail.com>
2024-10-01 21:15:37 +0700

    Add header and footer in common

--- common/header.php
+++ common/header.php
@@ -1,1 +1,13 @@
+<header>
+    <div class="name_web">
+    <h2 class="font-effect-shadow-multiple">KiHuLo</h2>
+    <h2 class="font-effect-shadow-multiple">Restaurant</h2>
+    </div>
+    <div class="search-bar">
+        <input type="text" placeholder="Search food..." />
+        <button>
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
+</header>
  
