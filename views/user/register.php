a1772cc49977919e2347bc743800a5c441a5a329
DLdevcoder <dtlong1410@gmail.com>
2024-10-01 17:05:23 +0700

    Add Sign Up page

--- views/user/register.php
+++ views/user/register.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="setting_sign_up.css" />
+    <link rel="stylesheet" href="signup.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple"
