77eb4e80b9bb7d0a87f2e3eaba957ca1fad95e20
DLdevcoder <dtlong1410@gmail.com>
2024-10-01 22:59:29 +0700

    Update footer

--- common/footer.php
+++ common/footer.php
@@ -1,1 +1,19 @@
- 
+<footer>
+      <table class="info">
+        <tr>
+          <th><strong>ADDRESS</strong></th>
+          <th>OPEN HOUR</th>
+          <th>CONTACT</th>
+        </tr>
+        <tr>
+          <td>Hà Nội-Việt Nam</td>
+          <td>Monday-Sunday</td>
+          <td>Tel:0xxxxxxxx</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>All day from: 7:30am to 23:00</td>
+          <td>Gmail:KiHuLo@gmail.com</td>
+        </tr>
+      </table>
+</footer>
