5e5b358b661ab28dce981f370410195ec7493561
Nguyen Trung Kien <kienlx2005@gmail.com>
2024-10-02 20:16:27 +0700

    trash

--- views/user/menu.php
+++ views/user/menu.php
@@ -1,1 +1,15 @@
- 
+<body>
+    <?php while ($row = $result->fetch_assoc()){?>
+    <table>
+        <tr>
+            <td><?php echo $row['item_name'];?></td>
+            <td><?php echo $row['price'];?></td>
+            <td><?php echo $row['category_id'];?></td>
+            <td><img src="<?php echo $row['image_url']; ?>" alt="<?php echo $row['item_name']; ?>" style="width:100px;height:100px;"></td>
+        </tr>
+    </table>
+
+
+    <?php } ?>
+</body>
+</html>
