d3c024282039553869fe81699fe7389dbe917425
DLdevcoder <dtlong1410@gmail.com>
2024-10-02 21:51:56 +0700

    Fix register

---
+++ common/sidebarAD.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+    <form action="" method="POST"   >
+  <div class="sidebar">
+        <h2>Dashboard</h2>
+        <ul>
+            <li><a href="#">Quản lí người dùng <span class="notification">2</span></a></li>
+            <li><a href="#">Quản lí thực đơn <span class="notification">2</span></a></li>
+            <li><a href="#">Quản lí đơn hàng <span class="notification">0</span></a></li>
+            <li><a href="#">Quản lí giao hàng <span class="notification">1</span></a></li>
+            <li><a href="#">Quản lí thanh toán <span class="notification">0</span></a></li>
+           
+        </ul>
+    </div>
+</form>
+
+
+  
+</body>
+</html>
