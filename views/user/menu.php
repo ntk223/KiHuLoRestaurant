da478e22fb24b0957d5e1bcfe22cba4138e9aa5d
DLdevcoder <dtlong1410@gmail.com>
2024-10-05 15:55:32 +0700

    add effect for menu

--- views/user/menu.php
+++ views/user/menu.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="assets/css/menu.css">
     <div class="menu">
+        <h1 style="margin: 10px;">Menu</h1>
     <?php while ($row = $result->fetch_assoc()){?>
         <div class="food">
+        <img src="<?php echo $row['image_url']; ?>" alt="<?php echo $row['item_name']; ?>" style="widli:200px;height:170px;">
         <ul>
-            <li><img src="<?php echo $row['image_url']; ?>" alt="<?php echo $row['item_name']; ?>" style="widli:200px;height:150px;"></li>
             <li>Tên món: <?php echo $row['item_name'];?></li>
             <li>Giá: <?php echo $row['price'];?> VNĐ</li>
             <li>Loại: <?php if($row['category_id'] == 1) echo 'Món chính';
