02e56070c7ed47de444640a5618d430b0e1c94df
DLdevcoder <dtlong1410@gmail.com>
2024-10-08 22:14:17 +0700

    Update effect sidebar,header

--- views/user/sidebar.php
+++ views/user/sidebar.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" href="assets/css/sidebar.css" />
 <div class="sidebar">
         <ul>
-            <li><a href="index.php?role=customer&page=menu&cate=all">Tất cả</a></li>
+            <li style="background-color: #f8a853;
+                color: #fff;"><a href="index.php?role=customer&page=menu&cate=all">Tất cả</a>
+            </li>
             <li><a href="index.php?role=customer&page=menu&cate=1">Món chính</a></li>
             <li><a href="index.php?role=customer&page=menu&cate=2">Khai vị</a></li> 
             <li><a href="index.php?role=customer&page=menu&cate=3">Tráng miệng</a></li>
