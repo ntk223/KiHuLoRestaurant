e6d3577ad6703d1a60b7f0785a2f45cc9eeb583f
DLdevcoder <dtlong1410@gmail.com>
2024-10-09 12:07:18 +0700

    Update font family

--- views/user/menu.php
+++ views/user/menu.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" href="assets/css/main.css">
 <link rel="stylesheet" href="assets/css/menu.css">
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Handlee&display=swap" rel="stylesheet">
+
+
 <main>
   <div class="menu">
     <div style="width : 100%">
-      <h1 style="margin: 10px;">
+      <h1 style="margin: 10px;font-family: 'Dancing Script', cursive;font-size:50px">
         <?php if(isset($_GET['cate'])){
           if($_GET['cate'] == 'all') echo 'Tất cả';
           elseif($_GET['cate'] == 1) echo 'Món chính';
@@ -18,7 +22,7 @@
       <div class="food">
       <img src="<?php echo $row['image_url']; ?>" alt="<?php echo $row['item_name']; ?>" style="width:200px;height:170px;">
       <ul>
-          <li>Tên món: <?php echo $row['item_name'];?></li>
+          <li>Tên món: <span style="font-family: 'Dancing Script', cursive;font-size:23px"><?php echo $row['item_name'];?></span></li>
           <li>Giá: <?php echo $row['price'];?> VNĐ</li>
           <li>Loại: <?php if($row['category_id'] == 1) echo 'Món chính';
                           elseif($row['category_id'] == 2) echo 'Món khai vị';
