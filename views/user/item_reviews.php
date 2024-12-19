0511f357885212c616e17635c7ebb02cbed2fbd2
Nguyen Trung Kien <kienlx2005@gmail.com>
2024-12-19 23:47:58 +0700

    fixx

--- views/user/item_reviews.php
+++ views/user/item_reviews.php
@@ -33,11 +33,11 @@
 <form class="review-form" action="index.php?role=customer&page=review&action=add&item_id=<?php echo $_GET['item_id']; ?>" method="POST">
     <label for="rating">Đánh giá:</label>
     <div class="star-rating">
-        <input type="radio" id="star5" name="rating" value="5" /><label for="star5">★</label>
-        <input type="radio" id="star4" name="rating" value="4" /><label for="star4">★</label>
-        <input type="radio" id="star3" name="rating" value="3" /><label for="star3">★</label>
-        <input type="radio" id="star2" name="rating" value="2" /><label for="star2">★</label>
-        <input type="radio" id="star1" name="rating" value="1" /><label for="star1">★</label>
+        <input type="radio" id="star5" name="rating" value="5" required /><label for="star5">★</label>
+        <input type="radio" id="star4" name="rating" value="4" required /><label for="star4">★</label>
+        <input type="radio" id="star3" name="rating" value="3" required /><label for="star3">★</label>
+        <input type="radio" id="star2" name="rating" value="2" required /><label for="star2">★</label>
+        <input type="radio" id="star1" name="rating" value="1" required /><label for="star1">★</label>
     </div>
     
     <label for="comment">Nhận xét:</label>
@@ -45,5 +45,4 @@
     
     <button type="submit" name="submit">Gửi review</button>
 </form>
-    
 </body>
