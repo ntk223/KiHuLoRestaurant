33bd5f883ce3fbe0221a236928f2c965906cab7b
Nguyen Trung Kien <kienlx2005@gmail.com>
2024-12-19 22:49:42 +0700

    fixx

--- common/header.php
+++ common/header.php
@@ -1,5 +1,7 @@
-<?php ob_start();?>
-
+<?php
+ob_start(); // Bắt đầu output buffering
+?>
+<header>
 <link rel="stylesheet" href="assets/css/headerandfooter.css" />
 <link rel="stylesheet" href="assets/css/menu.css"/>
 
@@ -7,38 +9,42 @@
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple"
     />
-<link
+    <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
     />
-<div class="name_web">
-    <h2 class="font-effect-shadow-multiple">KiHuLo</h2>
-    <h2 class="font-effect-shadow-multiple">Restaurant</h2>
-</div>
-<div class="contact">
-    <a href="https://www.facebook.com/">
-        <img src="assets/images/facebook.png" alt="icon facebook" />
-    </a>
-    <a href="https://www.instagram.com/">
-        <img src="assets/images/instagram.png" alt="icon instagram" />
-    </a>
-    <a href="https://www.youtube.com/">
-        <img src="assets/images/youtube.png" alt="icon youtube" />
-    </a>
-</div>
-<div class="search-bar">
-    <?php include_once "searchbar.php"; ?>
-</div>
-<div class="users">
-    <div class="users-info">
-        <img src="assets/images/user-img.png" alt="user-img" />
-        <p class="user-name"><?php echo $_SESSION['username']?></p>
-        <div class="setting-user">
+      <div class="name_web">
+        <h2 class="font-effect-shadow-multiple">KiHuLo</h2>
+        <h2 class="font-effect-shadow-multiple">Restaurant</h2>
+      </div>
+      <div class="contact">
+        <a href="https://www.facebook.com/">
+          <img src="assets/images/facebook.png" alt="icon facebook" />
+        </a>
+        <a href="https://www.instagram.com/">
+          <img src="assets/images/instagram.png" alt="icon instagram" />
+        </a>
+        <a href="https://www.youtube.com/">
+          <img src="assets/images/youtube.png" alt="icon youtube" />
+        </a>
+      </div>
+      <div class="search-bar">
+        <?php include_once "searchbar.php"; ?>
+      </div>
+      <div class="users">
+        <div class="users-info">
+          <img src="assets/images/user-img.png" alt="user-img" />
+          <p class="user-name"></p>
+          
+          <div class="setting-user">
             <a href="index.php?role=customer&page=profile">Thông tin</a>
             <a href="index.php?role=customer&page=password">Đổi mật khẩu</a>
             <a href="index.php?in=logout">Đăng xuất</a>
+          </div>
         </div>
-    </div>
-</div>
-<?php include("views/user/navbar.php");?>
-<?php ob_end_flush();?>
+      </div>
+      <?php include("views/user/navbar.php");?>
+</header>
+<?php
+ob_end_flush(); // Gửi tất cả output ra trình duyệt sau khi PHP hoàn thành
+?>
