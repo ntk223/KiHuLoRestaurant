9352297d88c444f6751c8a0754aacbdac4da3dd4
Nguyen Trung Kien <kienlx2005@gmail.com>
2024-12-19 22:00:27 +0700

    update

--- views/user/login.php
+++ views/user/login.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/KiHuLoRestaurant/assets/css/login.css" />
+    <link rel="stylesheet" href="assets/css/login.css" />
     <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple"
